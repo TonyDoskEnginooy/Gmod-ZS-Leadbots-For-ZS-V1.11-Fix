156ef6d5268b18297e00172194a24639644a95d8
Xalalau <xalalau@outlook.com>
2026-04-11 23:25:01 -0300

    feat: add zs_coasthouse func_breakable removals

--- lua/leadbot/gamemodes/zs/sv/map_handler.lua
+++ lua/leadbot/gamemodes/zs/sv/map_handler.lua
@@ -223,7 +223,12 @@ ZSB.Map.handler = {
     },
     zs_coasthouse = {
         campingSpotList = { Vector(-97.21, 167.99, 264.03), Vector(-759.96, -87.85, 392.38), Vector(-331.66, 167.96, 392.03) },
-        eyeAngles = function(strategy, doorVar, hallVar, openVar) return Angle(0, ({ 270 + doorVar, 0 + hallVar, 270 + hallVar })[strategy], 0) end
+        eyeAngles = function(strategy, doorVar, hallVar, openVar) return Angle(0, ({ 270 + doorVar, 0 + hallVar, 270 + hallVar })[strategy], 0) end,
+        removeFuncBreakable = true,
+        removeFuncBreakableFilter = function(v)
+            local modelName = v:GetModel()
+            return modelName == "*5" or modelName == "*6" or modelName == "*7"
+        end,
     },
     zs_lakefront_alpha = {
         campingSpotList = { Vector(1463.96, 1879.96, 14.03), Vector(1248.63, 1820.72, 14.03), Vector(1090.03, 1819.96, 14.03) },
