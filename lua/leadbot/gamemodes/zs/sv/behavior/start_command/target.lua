1ce055a8fcce1d8f8195b26588347f8b1ebb5586
Xalalau <xalalau@outlook.com>
2026-04-14 13:21:24 -0300

    refactor: move some files and code

--- lua/leadbot/gamemodes/zs/sv/behavior/start_command/target.lua
+++ lua/leadbot/gamemodes/zs/sv/behavior/start_command/target.lua
@@ -268,3 +268,38 @@ function SC.ForgetInvalidTarget(bot, controller)
         return
     end
 end
+
+function SC.UpdateGoalFromTarget(bot, controller)
+    if not IsValid(controller.Target) then return end
+
+    local PosGen = ZSB.Util.GetPos(controller.Target, bot:GetPos())
+
+    if controller.PosGen == PosGen then return end
+
+    if bot:IsPlayer() and controller.Target:IsPlayer() and (
+        bot:Team() ~= controller.Target:Team()
+        or (bot:Team() == TEAM_SURVIVORS and controller.Target:IsNPC())
+    ) then
+        controller.PosGen = PosGen
+    elseif bot:Team() == TEAM_SURVIVORS and SC.IsSurvivorBreakTarget(bot, controller.Target) then
+        controller.PosGen = PosGen
+    end
+end
+
+function SC.ClearGoal(controller)
+    if not IsValid(controller) then
+        return
+    end
+
+    controller.PosGen = nil
+    controller.TPos = nil
+    controller.ForgetTarget = 0
+    controller.CurSegmentIndex = 2
+    controller.GoalPos = vector_origin
+    controller.NextStrafe = 0
+    controller.NextJump = -1
+    controller.NextRandomJump = 0
+    controller.NextDuck = 0
+    controller.NextDuckCheck = 0
+    controller.StrafeAngle = 0
+end
