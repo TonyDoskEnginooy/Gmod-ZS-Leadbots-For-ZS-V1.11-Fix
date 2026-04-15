345f517f356d0de03942831efb2109db458083bd
Xalalau <xalalau@outlook.com>
2026-04-14 21:52:36 -0300

    chore: block zombies from seeking unbreakable props

--- lua/leadbot/gamemodes/zs/sv/behavior/start_command/zombie_destruction.lua
+++ lua/leadbot/gamemodes/zs/sv/behavior/start_command/zombie_destruction.lua
@@ -54,8 +54,8 @@ function SC.AcquireZombieBreakTarget(bot, controller, foundEnts)
 
     ConsiderBestTarget(bot, controller, state, foundEnts.near["func_breakable"], "func_breakable", ScoreZombieObstacleTarget)
     ConsiderBestTarget(bot, controller, state, foundEnts.near["func_physbox"], "func_physbox", ScoreZombieObstacleTarget)
-    ConsiderBestTarget(bot, controller, state, foundEnts.near["prop_physics"], "prop_physics", ScoreZombieObstacleTarget)
-    ConsiderBestTarget(bot, controller, state, foundEnts.near["prop_dynamic"], "prop_dynamic", ScoreZombieObstacleTarget)
+    --ConsiderBestTarget(bot, controller, state, foundEnts.near["prop_physics"], "prop_physics", ScoreZombieObstacleTarget)
+    --ConsiderBestTarget(bot, controller, state, foundEnts.near["prop_dynamic"], "prop_dynamic", ScoreZombieObstacleTarget)
 
     if IsValid(state.bestTarget) then
         controller.Target = state.bestTarget
